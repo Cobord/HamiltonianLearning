c251492ea0224e804c533dc33e73e2aecab04901
Ammar Husain <ammar.s.husain@gmail.com>
2017-12-26 19:49:25 -0800

    Null Space
    Added null space, gives -.5*(usual equation) which is fine

--- varietyFitter.py
+++ varietyFitter.py
@@ -2,6 +2,17 @@ import matplotlib.pyplot as plt
 #import tensorflow as tf
 from sklearn.preprocessing import PolynomialFeatures
 
+import scipy
+import numpy as np
+from scipy import linalg, matrix
+
+def null(A,eps=1e-9):
+    u, s, vh = scipy.linalg.svd(A)
+    padding = max(0,np.shape(A)[1]-np.shape(s)[0])
+    null_mask = np.concatenate(((s <= eps), np.ones((padding,),dtype=bool)),axis=0)
+    null_space = scipy.compress(null_mask, vh, axis=0)
+    return scipy.transpose(null_space)
+
 dataPoints = np.array([[1.0,0],
                         [3.0/5,4.0/5],
                         [4.0/5,3.0/5],
@@ -17,12 +28,11 @@ plt.show()
 
 poly= PolynomialFeatures(degree=2)
 monomials=poly.fit_transform(dataPoints)
-print(monomials)
+#print(monomials)
 
-#TODO: still have to calculate nullspace of the monomials matrix
-#that gives the quadratic that best fits this variety
-# it should be -1*1+0*x+0*y+1*x^2+0*xy+1*y^2
-# copy nullspace code from https://stackoverflow.com/questions/5889142/python-numpy-scipy-finding-the-null-space-of-a-matrix
+defining_equation=null(monomials)
+print(defining_equation)
+# it should be -1*1+0*x+0*y+1*x^2+0*xy+1*y^2 or something proportional
 
 #TODO: do same for other plane curves of higher degree to see how well it scales
-#TODO: noise the data so no longer exactly null space
+#TODO: noise the data so no longer exactly null space, idea randomly select subsets
