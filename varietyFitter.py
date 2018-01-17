fe0d75e3a464b821be1ba653e45e3996eeef56b1
Ammar Husain <ammar.s.husain@gmail.com>
2018-01-17 11:43:31 -0800

    Noisy
    Noise the data and give lowest num singular vectors instead of ones with eps cutoff

--- varietyFitter.py
+++ varietyFitter.py
@@ -5,6 +5,21 @@ import scipy
 import numpy as np
 from scipy import linalg, matrix
 
+def lowestEigenspaces(A,num=1):
+    u, s, vt = scipy.linalg.svd(A)
+    n = len(s)
+    # reverse the n first columns of u
+    u[:,:n] = u[:, n-1::-1]
+    # reverse s
+    s = s[::-1]
+    # reverse the n first rows of vt
+    vt[:n, :] = vt[n-1::-1, :]
+    #print(vt)
+    #print(vt[0:num])
+    #print(vt[0:num,:])
+    null_space = vt[0:num]
+    return scipy.transpose(null_space)
+
 def null(A,eps=1e-9):
     u, s, vh = scipy.linalg.svd(A)
     padding = max(0,np.shape(A)[1]-np.shape(s)[0])
@@ -20,6 +35,9 @@ dataPoints = np.array([[1.0,0],
                         [0,-1],
                         ])
 
+#noise the data so no longer exactly a circle
+dataPoints = dataPoints + .05*np.random.randn(6,2)
+
 plt.scatter(dataPoints[:,0],dataPoints[:,1])
 plt.gca().set_aspect('equal')
 plt.savefig('circleTestCase.png')
@@ -29,9 +47,11 @@ poly= PolynomialFeatures(degree=2)
 monomials=poly.fit_transform(dataPoints)
 #print(monomials)
 
-defining_equation=null(monomials)
+# the .1 here is chosen to make the threshold instead of exact null space
+#defining_equation=null(monomials,.1)
+defining_equation=lowestEigenspaces(monomials)
 print(defining_equation)
 # it should be -1*1+0*x+0*y+1*x^2+0*xy+1*y^2 or something proportional
 
 #TODO: do same for other plane curves of higher degree to see how well it scales
-#TODO: noise the data so no longer exactly null space, idea randomly select subsets
+# in planeCurves.py
